adca4ea34e1b6de3687ca2c61d39b7c8713dd2d7
聚指尖 <princergx@live.com>
2021-12-09 22:10:05 +0800

    更新：community.applications

--- community.applications/main.sh
+++ community.applications/main.sh
@@ -9,14 +9,12 @@ git config --global user.email "admin@juzhijian.com"
 
 echo 开始替换V$version
 
-mkdir -p "/tmp/GitHub/community.applications/source/community.applications/"
+mkdir -p "/tmp/GitHub/community.applications/source/community.applications/usr/local/emhttp/plugins/community.applications/"
 cd archive
-ls
 tar xJvf community.applications-${version}-x86_64-1.txz
-#cd /tmp/GitHub/community.applications/source/community.applications/
+cp /github/workspace/community.applications/archive/usr/local/emhttp/plugins/community.applications/* /tmp/GitHub/community.applications/source/community.applications/usr/local/emhttp/plugins/community.applications -R -v -p
+rm -rf /usr/*
 ls
-
-#cp /github/workspace/community.applications/source/community.applications/usr/local/emhttp/plugins/community.applications/* /tmp/GitHub/community.applications/source/community.applications/usr/local/emhttp/plugins/community.applications -R -v -p
 #cd /tmp/GitHub/community.applications/source/community.applications/usr/local/emhttp/plugins/community.applications
 #rm -f  ca.md5
 #find . -type f -exec md5sum {} + > /tmp/ca.md5
