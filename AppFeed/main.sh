8538d5ab9e0cea94bd5a11e81ffb0992892b76d8
聚指尖 <princergx@live.com>
2021-12-09 20:46:37 +0800

    更新：AppFeed

--- AppFeed/main.sh
+++ AppFeed/main.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-###
- # @Author: your name
- # @Date: 2021-12-09 20:43:13
- # @LastEditTime: 2021-12-09 20:45:21
- # @LastEditors: your name
- # @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- # @FilePath: \SourceTree\Unraid\UnraidAPP\AppFeed\main.sh
-### 
 echo 运行替换程序成功
 
 git config --global user.name "聚指尖"
