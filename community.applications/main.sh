ec70d66a349f199ea284b872f3cf55133595fbd9
聚指尖 <princergx@live.com>
2021-12-09 21:44:19 +0800

    更新：community.applications

---
+++ community.applications/main.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+versions=2021.12.08
+
+echo 运行替换程序成功
+
+git config --global user.name "聚指尖"
+git config --global user.email "admin@juzhijian.com"
+
+echo 开始替换V$versions
+
+
+
+mkdir -p "/tmp/GitHub/community.applications/source/community.applications/usr/local/emhttp/plugins/community.applications/"
+
+cp /github/workspace/community.applications/source/community.applications/usr/local/emhttp/plugins/community.applications/* /tmp/GitHub/community.applications/source/community.applications/usr/local/emhttp/plugins/community.applications -R -v -p
+cd /tmp/GitHub/community.applications/source/community.applications/usr/local/emhttp/plugins/community.applications
+rm -f  ca.md5
+find . -type f -exec md5sum {} + > /tmp/ca.md5
+mv /tmp/ca.md5 ca.md5
+
+
+echo 替换结束
+
+echo 开始打包
+
+echo 打包结束
