56a23aeac877504ebccc0203730971802a28596a
聚指尖 <admin@juzhijian.com>
2021-12-09 13:44:43 +0800

    更新：AppFeed

--- AppFeed/main.sh
+++ AppFeed/main.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 echo 运行替换程序成功
+
+git config --global user.name "聚指尖"
+git config --global user.email "admin@juzhijian.com"
+
 echo 测试开始搜索文件
 find / -name "applicationFeed.json"
 pwd
 echo 开始替换
-sed -i  "s/"https://raw.githubusercontent.com/Squidly271/ca.update.applications/master/"/"https://juzhijian.coding.net/p/unraidapp/d/ca.update.applications/git/raw/master/"/g"  `grep "https://juzhijian.coding.net/p/unraidapp/d/ca.update.applications/git/raw/master/" -rl ./ -R --exclude-dir=.git`
+#sed -i  "s/"https://raw.githubusercontent.com/Squidly271/ca.update.applications/master/"/"https://juzhijian.coding.net/p/unraidapp/d/ca.update.applications/git/raw/master/"/g"  `grep "https://juzhijian.coding.net/p/unraidapp/d/ca.update.applications/git/raw/master/" -rl ./ -R --exclude-dir=.git`
+
+mkdir asjijhk
+
+
+
 echo 运行结束
