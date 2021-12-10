76d54489adf1b338ed47c66bb0a657fef46a5c73
聚指尖 <admin@juzhijian.com>
2021-12-10 13:26:02 +0800

    更新：AppFeed

--- AppFeed/main.sh
+++ AppFeed/main.sh
@@ -2,13 +2,16 @@
 
 echo 运行替换程序成功
 
+#=====设置git=====
 git config --global user.name "聚指尖"
 git config --global user.email "admin@juzhijian.com"
+#=================
 
 echo 测试开始搜索文件
 find / -name "applicationFeed.json"
 pwd
 ls
+
 echo 开始替换
 
 sed -i "s/https:\/\/raw.githubusercontent.com\/Squidly271\/ca.update.applications\/master\//https:\/\/juzhijian.coding.net\/p\/unraidapp\/d\/ca.update.applications\/git\/raw\/master\//g" $(grep "https://raw.githubusercontent.com/Squidly271/ca.update.applications/master/" -rl ./)
