7c0e25a662fa3da184b391b070487cd6348f1c97
聚指尖 <admin@juzhijian.com>
2021-12-10 11:26:55 +0800

    更新：AppFeed

--- AppFeed/main.sh
+++ AppFeed/main.sh
@@ -11,8 +11,8 @@ pwd
 ls
 echo 开始替换
 
-sed -i  "s/https:\/\/raw.githubusercontent.com\/Squidly271\/ca.update.applications\/master\//https:\/\/juzhijian.coding.net\/p\/unraidapp\/d\/ca.update.applications\/git\/raw\/master\//g"  `grep "https://raw.githubusercontent.com/Squidly271/ca.update.applications/master/" -rl ./`
+sed -i "s/https:\/\/raw.githubusercontent.com\/Squidly271\/ca.update.applications\/master\//https:\/\/juzhijian.coding.net\/p\/unraidapp\/d\/ca.update.applications\/git\/raw\/master\//g" $(grep "https://raw.githubusercontent.com/Squidly271/ca.update.applications/master/" -rl ./)
 
-sed -i  "s/https:\/\/raw.githubusercontent.com\/Squidly271\/plugin-repository\/master\//https:\/\/juzhijian.coding.net\/p\/unraidapp\/d\/plugin-repository\/git\/raw\/master\//g"  `grep "https://raw.githubusercontent.com/Squidly271/plugin-repository/master/" -rl ./`
+sed -i "s/https:\/\/raw.githubusercontent.com\/Squidly271\/plugin-repository\/master\//https:\/\/juzhijian.coding.net\/p\/unraidapp\/d\/plugin-repository\/git\/raw\/master\//g" $(grep "https://raw.githubusercontent.com/Squidly271/plugin-repository/master/" -rl ./)
 
 echo 替换结束
