944258213ac609c108ad95ec6c4a2ea3ea99a322
juzhijian <princergx@live.com>
2023-02-24 16:10:23 +0800

    更新：AppFeed

--- AppFeed/main.sh
+++ AppFeed/main.sh
@@ -36,23 +36,31 @@ sed -i "s/https:\/\/raw.githubusercontent.com\/Squidly271\/fix.common.problems\/
 sed -i "s/https:\/\/raw.githubusercontent.com\/bergware\/dynamix\/master\//https:\/\/juzhijian.coding.net\/p\/unraidapp\/d\/dynamix\/git\/raw\/master\//g" $(grep "https://raw.githubusercontent.com/bergware/dynamix/master/" -rl ./)
 sed -i "s/https:\/\/raw.github.com\/bergware\/dynamix\/master\//https:\/\/juzhijian.coding.net\/p\/unraidapp\/d\/dynamix\/git\/raw\/master\//g" $(grep "https://raw.github.com/bergware/dynamix/master/" -rl ./)
 
+echo 删除旧.git文件
+rm -rf .git
+
+echo 初始化仓库
+git init
+
+
+
 
 # 创建新分支 "latest_branch" 并转到该分支
 # git checkout 命令为在当前分支上创建新分支(并转到该分支)
 # git checkout --orphan 命令为从空白处创建新分支(并转到该分支)
-git checkout --orphan latest_branch
+#git checkout --orphan latest_branch
 
 # 添加所有文件(当前最新状态)到暂存区
-git add -A
+#git add -A
 
 # 将暂存区提交到新纪录(latest_branch分支)
 # "commit message"为新纪录的名称
-git commit -am "commit message"
+#git commit -am "commit message"
 
 # 删除主分支
-git branch -D master
+#git branch -D master
 
 # 将latest_branch(当前)分支重命名为master(主分支)
-git branch -m master
+#git branch -m master
 
 echo 替换结束
