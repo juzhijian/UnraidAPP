b10b33d99e6427ac8794efe3982f6bbd4f6c43a2
juzhijian <princergx@live.com>
2023-02-25 15:44:10 +0800

    更新：unRAID-NerdPack

--- unRAID-NerdPack/main.sh
+++ unRAID-NerdPack/main.sh
@@ -46,7 +46,7 @@ grep -rl "https://raw.githubusercontent.com/${AUTHOR}/${NAME}/master/" ./ | xarg
 
 cp /tmp/GitHub/${APPNAME}/source/NerdPack/* /github/workspace/${APPNAME}/source/NerdPack/ -R -v -p -f
 
-#=====更新plg=====
+#=====更新plg====
 cd /github/workspace/${APPNAME}/plugin
 
 sed -i "8c <!ENTITY gitURL    \"https://gitcode.net/juzhijian/unRAID-&name;/-/raw/master\">" NerdPack.plg
