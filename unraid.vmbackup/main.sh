68f9db8abbfe3f66afe871bcb71224997b41a701
juzhijian <princergx@live.com>
2023-06-03 01:09:36 +0800

    更新：community.applicationsUpdate

--- unraid.vmbackup/main.sh
+++ unraid.vmbackup/main.sh
@@ -19,19 +19,6 @@ git config --global user.name "juzhijian"
 git config --global user.email "princergx@live.com"
 #=================
 
-#=====替换链接====
-echo 开始替换V${version}
-
-mkdir -p "/tmp/GitHub/${APPNAME}/source/${APPNAME}/usr/local/emhttp/plugins/${APPNAME}/"
-
-cd archive
-tar xJvf ${APPNAME}-${version}.txz
-cp /github/workspace/${APPNAME}/archive/usr/local/emhttp/plugins/${APPNAME}/* /tmp/GitHub/${APPNAME}/source/${APPNAME}/usr/local/emhttp/plugins/${APPNAME} -R -v -p
-rm -rf usr
-cd /tmp/GitHub/${APPNAME}/source/${APPNAME}/usr/local/emhttp/plugins/${APPNAME}
-
-cp /tmp/GitHub/${APPNAME}/source/${APPNAME}/usr/local/emhttp/plugins/${APPNAME}/* /github/workspace/${APPNAME}/source/${APPNAME}/usr/local/emhttp/plugins/${APPNAME} -R -v -p -f
-
 #=====更新plg====
 cd /github/workspace/${APPNAME}/
 
@@ -40,29 +27,4 @@ sed -i "10,11c <!ENTITY github \"juzhijian/${APPNAME}/-/raw\">\n<!ENTITY pluginU
 sed -i "s/raw.githubusercontent.com\//jtok/gitcode.net\//juzhijian/g" $(grep "raw.githubusercontent.com\//jtok" -rl ./)
 
 echo 替换结束
-#=================
-
-
-#=====开始打包====
-echo 开始打包
-
-cd /tmp/GitHub/${APPNAME}/source/${APPNAME}/
-
-chmod 0755 -R .
-
-echo "7zip压缩"
-7z a -ttar -r ${APPNAME}-${version}.tar usr/
-7z a -txz -r ${APPNAME}-${version}.txz ${APPNAME}-${version}.tar
-echo "7zip压缩完成"
-
-cp /tmp/GitHub/${APPNAME}/source/${APPNAME}/${APPNAME}-${version}.txz /github/workspace/${APPNAME}/archive/ -R -v -p -f
-
-md5_data=$(md5sum /github/workspace/${APPNAME}/archive/${APPNAME}-${version}.txz | cut -d ' ' -f1)
-
-echo "md5_data = ${md5_data}"
-
-cd /github/workspace/${APPNAME}/plugins
-sed -i "6c <!ENTITY md5       "\"${md5_data}\"">" ${APPNAME}.plg
-
-echo 打包结束
 #=================
